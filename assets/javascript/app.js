e0027e21d34c8e94cc1ee470efbf4c944b68966d
Stangle <RStangle@firstam.com>
2017-08-20 13:14:10 -0400

    Pulling specific subject matter from API and Buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,7 +2,7 @@
 
 //THINGS TO DO
 	// Obtain API Key
-	// Pick a subject for giffs --- sci-fi, guardians of the galaxy, star trek, star wars, babylon 5, x-men, alien, battlestar galactica, fifth element
+	// Pick a subject for gifs --- sci-fi, guardians of the galaxy, star trek, star wars, babylon 5, x-men, alien, battlestar galactica, fifth element
 	// AJAX to the giphy site
 	// Create array of strings related to sci-fi topic
 		// Save to var topics
@@ -31,15 +31,27 @@
 
 // FUNCTIONS
 
-	// Check to see if onclick is working with data-name... so far it is.
+	// Check to see if onclick is working with data-name with each button... so far it is.
+	// I don't understand why this worked and the other ones below did not, but I'll go with this. Throwing speghetti sometimes works... for now
 	function alertGifName() {
-		var gifName = $(this).attr("data-name");
-		alert(gifName);
-	}
+		var gifTopic = $(this).attr("data-name");
+		// alert(gifName);
 
-	// Check to see if onclick is working with data-name... so far it is.
-	$(document).on("click", ".category", alertGifName);
-	
+		// Assign the Giphy search URL to var queryURL with rating of g, limit of 10 gifs, my API key, with search of var gif, which is data-name attribute
+		var queryURL = "https://api.giphy.com/v1/gifs/search?rating=g&limit=10&api_key=3f4ef80b7a864d26acda3fda38bfde9e&q=" + gifTopic;
+		// console.log(gif);
+		console.log(queryURL);
+
+		$.ajax({
+			url: queryURL,
+			method: "GET"
+		}).done(function(response) {
+			console.log(response);
+		});
+	};
+
+
+
 //************************************************************************************************************************************************
 
 	// Render buttons from the topics []
@@ -52,8 +64,11 @@
 			a.attr("data-name", topics[i]); // Add data-attribute with value of the topic at current index of loop
 			a.text(topics[i]); // Button label
 			$("#category-tag").append(a);
+
+			console.log(a);
 		}
 	}
+
 
 //*************************************************************************************************************************************************
 
@@ -71,19 +86,63 @@
 
 //************************************************************************************************************************************************
 
-	function displayGif() {
-		var gif = $("this").attr("data-name");
-		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gif + "&rating=g&api_key=3f4ef80b7a864d26acda3fda38bfde9e&limit=10";
-		// console.log(gif);
-		// console.log(queryURL);
+	// $(".category").on("click", function(){
+	// 	var gifTopic = $(this).attr("data-name");  // This means on the mouse click of 'this' current clicked button, pull the data-name attribute and assign it var gif  
 
-		$.ajax({
-			url: queryURL,
-			method: "GET"
-		}).done(function(response) {
-			console.log(response);
-			// 	// Create a div to put the gifs in and class of 'category' from renderButton var a
-				var gifDiv = $("<div class='category'>");
+	// 	// Assign the Giphy search URL to var queryURL with rating of g, limit of 10 gifs, my API key, with search of var gif, which is data-name attribute
+	// 	var queryURL = "https://api.giphy.com/v1/gifs/search?rating=g&limit=10&api_key=3f4ef80b7a864d26acda3fda38bfde9e&q=" + gifTopic;
+	// 	// console.log(gif);
+	// 	console.log(queryURL);
+
+	// 	$.ajax({
+	// 		url: queryURL,
+	// 		method: "GET"
+	// 	}).done(function(response) {
+	// 		console.log(response);
+	// 		// var results = response.data;
+
+	// 		// for(var n = 0; n < results.length; n++ ) {
+	// 		// 	if (results[n].rating !== "r") {
+
+	// 		// 		var gifDiv = $("<div class='item'>");
+
+	// 		// 		var rating = results[n].rating;
+	// 		// 		var p = $("<p>").text("Rating: " + rating);
+
+	// 		// 		var gifImage = $("<img>");
+
+	// 		// 		gifImage.attr("src", results[n].images.fixed_height.url);
+
+	// 		// 		gifDiv.append(p);
+	// 		// 		gifDiv.append(gifImage);
+
+	// 		// 		$("#result-gifs").prepend(gifDiv);
+	// 		// 	}
+	// 		// }
+
+	// 	});
+	// });
+	
+
+	// function displayGif() {
+	// 	var gif = $(this).attr("data-name"); // Do I need to replace ".category" with "this"? 
+	// 	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gif + "&rating=g&api_key=3f4ef80b7a864d26acda3fda38bfde9e&limit=10";
+	// 	// console.log(gif);
+	// 	// console.log(queryURL);
+
+	// 	$.ajax({
+	// 		url: queryURL,
+	// 		method: "GET"
+	// 	}).done(function(response) {
+	// 		console.log(response);
+	// 		// 	// Create a div to put the gifs in and class of 'category' from renderButton var a
+	// 			var gifDiv = $("#result-gifs");
+
+	// 			var rating = response.data.rating;
+
+	// 			var pRating = $("<p>").text("Rating: " + rating);
+
+	// 			gifDiv.append(pRating);
 
 
 
@@ -101,11 +160,12 @@
 				// Creates paragraph tag to put the rating in
 				// var p = $("<p>").text("This GIF rated: " + )
 
-			})
-		}
-	
+		
 
 	// }
+// Check to see if onclick is working with data-name with each button... so far it is.
+// Finally got this to pull the Giphy data and currently console.logging to view it.
+$(document).on("click", ".category", alertGifName);
 
 renderButtons();
-displayGif();
+// displayGif();
